83b14a612cb69967f7856281d603ca2e8aadd833
unknown <zahrafadhail@gmail.com>
2025-12-01 23:06:21 +0700

    editcart

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -22,7 +22,8 @@
     /* ===== CARD EFFECT: SOFT SHADOW & LIFT ===== */
     .card {
         box-shadow: 0 6px 20px rgba(0, 0, 0, 0.4) !important;
-        border-radius: 12px !important;
+        border-radius: 12px
+         !important;
     }
 
     .card:hover {
