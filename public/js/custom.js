5845e37bd848270d890a6d05f70a0e1bff6453a4
Deval <kadevakoharu@gmail.com>
2025-12-02 10:34:06 +0700

    ilangin login dan register card gerak2 biar ga pusing

--- public/js/custom.js
+++ public/js/custom.js
@@ -12,29 +12,4 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    // --- 2. ANIMASI 3D TILT LOGIN CARD ---
-    const loginCard = document.querySelector('.login-card');
-    const loginContainer = document.querySelector('.login-body');
-
-    if (loginCard && loginContainer) {
-        loginContainer.addEventListener('mousemove', (e) => {
-            const xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-            const yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-            
-            // Gerakkan kartu
-            loginCard.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-        });
-
-        // Saat mouse keluar, kembalikan posisi kartu
-        loginContainer.addEventListener('mouseleave', () => {
-            loginCard.style.transform = `rotateY(0deg) rotateX(0deg)`;
-            loginCard.style.transition = 'all 0.5s ease';
-        });
-
-        // Hapus transisi saat mouse masuk supaya gerakan smooth
-        loginContainer.addEventListener('mouseenter', () => {
-            loginCard.style.transition = 'none';
-        });
-    }
-
 });
